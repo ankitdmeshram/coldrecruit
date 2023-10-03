38e6a3d0c8eb4d49a02b19a945a7ec3ab46158e7
Ankit Meshram <ankitdm69@gmail.com>
2023-10-03 23:51:38 +0530

    job listing css

--- app/components/Jobs.tsx
+++ app/components/Jobs.tsx
@@ -9,22 +9,64 @@ export const Jobs = () => {
 
         <div className="gs-listing">
           <div className="gs-list">
-            <h1>Listing one</h1>
+            <div>
+              <h3>Full Stack Backend Developer</h3>
+              <p>Experience : 0-1 Years</p>
+            </div>
+            <div>
+              <h4>Company Name</h4>
+              <p className="gs-text-center"><button className="gs-btn-primary gs-mt-2">Apply Now</button></p>
+            </div>
           </div>
           <div className="gs-list">
-            <h1>Listing one</h1>
+            <div>
+              <h3>Front End Web Develoer</h3>
+              <p>Experience : 0-1 Years</p>
+            </div>
+            <div>
+              <h4>Company Name</h4>
+              <p className="gs-text-center"><button className="gs-btn-primary gs-mt-2">Apply Now</button></p>
+            </div>
           </div>
           <div className="gs-list">
-            <h1>Listing one</h1>
+            <div>
+              <h3>Backend Web Developer</h3>
+              <p>Experience : 0-1 Years</p>
+            </div>
+            <div>
+              <h4>Company Name</h4>
+              <p className="gs-text-center"><button className="gs-btn-primary gs-mt-2">Apply Now</button></p>
+            </div>
           </div>
           <div className="gs-list">
-            <h1>Listing one</h1>
+            <div>
+              <h3>Database Architecture</h3>
+              <p>Experience : 0-1 Years</p>
+            </div>
+            <div>
+              <h4>Company Name</h4>
+              <p className="gs-text-center"><button className="gs-btn-primary gs-mt-2">Apply Now</button></p>
+            </div>
           </div>
           <div className="gs-list">
-            <h1>Listing one</h1>
+            <div>
+              <h3>Devops Engineer</h3>
+              <p>Experience : 0-1 Years</p>
+            </div>
+            <div>
+              <h4>Company Name</h4>
+              <p className="gs-text-center"><button className="gs-btn-primary gs-mt-2">Apply Now</button></p>
+            </div>
           </div>
           <div className="gs-list">
-            <h1>Listing one</h1>
+            <div>
+              <h3>Web Developer</h3>
+              <p>Experience : 0-1 Years</p>
+            </div>
+            <div>
+              <h4>Company Name</h4>
+              <p className="gs-text-center"><button className="gs-btn-primary gs-mt-2">Apply Now</button></p>
+            </div>
           </div>
         </div>
       </div>
