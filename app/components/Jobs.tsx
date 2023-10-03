d8cfda0405b1461d42b55e10ee77834bf86afa86
Ankit Meshram <ameshram@shv360.com>
2023-10-03 23:00:27 +0530

    background image changes and job sectionstarte

---
+++ app/components/Jobs.tsx
@@ -0,0 +1,8 @@
+export const Jobs = () => {
+  return <>
+    <div className="gs-jobs">
+        <h2>Latest Jobs</h2>
+    </div>
+  </>;
+};
+
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,7 @@
 import Header from "~/components/Header";
 import homeStyle from "../styles/home.css";
+import jobsStyle from "../styles/jobs.css";
+import { Jobs } from "~/components/Jobs";
 const _index = () => {
   return (
     <>
@@ -11,6 +13,10 @@ const _index = () => {
           <button className="gs-btn-primary gs-mt-2">Starts Now</button>
         </div>
       </div>
+
+      <Jobs />
+
+
     </>
   );
 };
@@ -22,4 +28,8 @@ export const links = () => [
     rel: "stylesheet",
     href: homeStyle,
   },
+  {
+    rel: "stylesheet",
+    href: jobsStyle
+  }
 ];
