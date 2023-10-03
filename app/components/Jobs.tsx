83fb2de0fcb1c452915ef1072be704a5ae79b83b
Ankit Meshram <ameshram@shv360.com>
2023-10-03 23:22:18 +0530

    job listing css

--- app/components/Jobs.tsx
+++ app/components/Jobs.tsx
@@ -1,8 +1,33 @@
 export const Jobs = () => {
-  return <>
-    <div className="gs-jobs">
+  return (
+    <>
+      <div className="gs-jobs">
         <h2>Latest Jobs</h2>
-    </div>
-  </>;
-};
+        <p className="gs-text-center">
+          Here's the most recent job listed on the website.
+        </p>
 
+        <div className="gs-listing">
+          <div className="gs-list">
+            <h1>Listing one</h1>
+          </div>
+          <div className="gs-list">
+            <h1>Listing one</h1>
+          </div>
+          <div className="gs-list">
+            <h1>Listing one</h1>
+          </div>
+          <div className="gs-list">
+            <h1>Listing one</h1>
+          </div>
+          <div className="gs-list">
+            <h1>Listing one</h1>
+          </div>
+          <div className="gs-list">
+            <h1>Listing one</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
